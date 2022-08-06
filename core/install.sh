d8be200040afc0b798dc003171657aee80b15417
TURB0 <turbozhacker@gmail.com>
2022-08-07 03:56:26 +0500

    initial commit1

--- core/install.sh
+++ core/install.sh
@@ -5,10 +5,11 @@ echo " "
 echo -e "\e[1;91m   --------------------------------- \e[1;91m\e[0m"
 echo -e "\e[1;91m !!     DOWNLOADING REQUIREMENTS     !! \e[1;91m\e[0m"
 echo -e "\e[1;91m   --------------------------------- \e[1;91m\e[0m"
+echo " "
+echo " "
 sleep 2
 
 sudo apt install python3 -y
-
 pip3 install -r core/requirements.txt
 
 mv core/vid2img vid2img
@@ -18,5 +19,6 @@ echo " "
 echo -e "\e[1;92m   --------------------------------- \e[1;92m\e[0m"
 echo -e "\e[1;92m !!     Installation Successfull    !! \e[1;92m\e[0m"
 echo -e "\e[1;92m   --------------------------------- \e[1;92m\e[0m"
+echo " "
 echo -e "\n\n\e[1;96m [\e[0m\e[1;91m###\e[0m\e[1;96m]\e[0m\e[1;33m Run python3 vid2img -h \e[1;33m\e[0m\e[1;96m[\e[0m\e[1;91m###\e[0m\e[1;96m]\e[0m"
 
